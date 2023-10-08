648066f616a6462afce968c6e9bdf09f4497e237
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2023-10-08 11:00:33 +0530

    Adds install deps script

---
+++ development/scripts/install-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# use the correct version of node
+nvm use 18
+
+# initialise frappe bench
+bench init --skip-redis-config-generation frappe-bench
+cd frappe-bench
+
+# set the config
+bench set-config -g db_host mariadb
+bench set-config -g redis_cache redis://redis-cache:6379
+bench set-config -g redis_queue redis://redis-queue:6379
+bench set-config -g redis_socketio redis://redis-socketio:6379
+
+# set the config to developer mode
+bench set-config -g developer_mode true
