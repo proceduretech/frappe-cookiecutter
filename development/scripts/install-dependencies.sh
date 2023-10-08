c9e75aa167576f2a60ed87d78b887041d4579bec
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2023-10-08 20:21:16 +0530

    Use same redis instance for cache, queue and socketio

--- development/scripts/install-dependencies.sh
+++ development/scripts/install-dependencies.sh
@@ -9,9 +9,9 @@ cd frappe-bench
 
 # set the config
 bench set-config -g db_host mariadb
-bench set-config -g redis_cache redis://redis-cache:6379
-bench set-config -g redis_queue redis://redis-queue:6379
-bench set-config -g redis_socketio redis://redis-socketio:6379
+bench set-config -g redis_cache redis://redis:6379  # For production redis://redis-cache:6379
+bench set-config -g redis_queue redis://redis:6379  # For production redis://redis-queue:6379
+bench set-config -g redis_socketio redis://redis:6379  # For production redis://redis-socketio:6379
 
 # set the config to developer mode
 bench set-config -g developer_mode true
